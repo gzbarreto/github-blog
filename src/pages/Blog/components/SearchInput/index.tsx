4fd34764e41df25adfac0ba3ec2a423565dd4ea6
gabs <gabrielazbarreto@gmail.com>
2025-09-20 18:16:03 -0300

    feat(SearchInput): implement form validation and update input handling

--- src/pages/Blog/components/SearchInput/index.tsx
+++ src/pages/Blog/components/SearchInput/index.tsx
@@ -2,17 +2,34 @@ import { useContext } from "react"
 import { SearchInputContainer, SearchInputHeader } from "./styles"
 import { PostsContext } from "../../../../contexts/PostsContext"
 
+import * as z from "zod"
+import { zodResolver } from "@hookform/resolvers/zod"
+import { useForm } from "react-hook-form"
+
+const searchFormInputSchema = z.object({
+  query: z.string(),
+})
+
+type SearchFormInput = z.infer<typeof searchFormInputSchema>
+
 export function SearchInput() {
   const { posts } = useContext(PostsContext)
+  const { register, handleSubmit } = useForm<SearchFormInput>({
+    resolver: zodResolver(searchFormInputSchema),
+  })
+
+  function handleSearchPosts(data: SearchFormInput) {
+    console.log("query:", data.query)
+  }
 
   return (
-    <SearchInputContainer>
+    <SearchInputContainer onSubmit={handleSubmit(handleSearchPosts)}>
       <SearchInputHeader>
         <h3>Publicações</h3>
         <span>{posts.length} publicações</span>
       </SearchInputHeader>
 
-      <input type="text" placeholder="Buscar conteúdo" />
+      <input type="text" placeholder="Buscar conteúdo" {...register("query")} />
     </SearchInputContainer>
   )
 }
